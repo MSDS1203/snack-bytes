995af449920e5a57ea26b99a8a5d7fb3de3ff123
Sofia Azam <srazam5@gmail.com>
2024-11-12 22:24:37 -0500

    created login and signup page

--- snakeGame/home.js
+++ snakeGame/home.js
@@ -7,36 +7,22 @@ const firebaseConfig = {
     messagingSenderId: "692321315764",
     appId: "1:692321315764:web:82b4636424f2dc01dea0e1"
   };
-
+  
+  // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
-  const db = firebase.firestore();
   
   auth.onAuthStateChanged(user => {
     if (user) {
       document.getElementById('user-info').textContent = `Logged in as: ${user.email}`;
-      loadHighScores(user.uid);
     } else {
-      location.href = 'login.html';
+      location.href = 'login.html'; // Redirect to login page if not logged in
     }
   });
   
-
-  function loadHighScores(userId) {
-    db.collection('highScores').doc(userId).get()
-      .then(doc => {
-        if (doc.exists) {
-          const data = doc.data();
-          document.getElementById('score-snake').textContent = data.snake || 0;
-        } else {
-          console.log('No high scores found');
-        }
-      });
-  }
-  
   function logout() {
     auth.signOut().then(() => {
-      location.href = 'login.html';
+      location.href = 'login.html'; // Redirect to login page after logout
     });
   }
   
