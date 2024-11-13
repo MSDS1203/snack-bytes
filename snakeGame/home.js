823541c7b94d817b6a60102217965905a7ef4477
Sofia Azam <srazam5@gmail.com>
2024-11-13 00:25:56 -0500

    attempted to add username to database

--- snakeGame/home.js
+++ snakeGame/home.js
@@ -1,3 +1,8 @@
+import { doc, updateDoc } from "firebase/firestore";
+
+// Get a reference to the document you want to update
+const docRef = doc(db, "userScores", "srazam5@gmail.com");
+
 const userEmail = localStorage.getItem("userEmail");
 if (userEmail) {
   document.getElementById("user-email").textContent = userEmail;
@@ -5,7 +10,11 @@ if (userEmail) {
   document.getElementById("user-email").textContent = "No user logged in.";
 }
 
-function updateUserName(userName)
+
+async function updateUserName(userName)
 {
-  
+  // Update the document field
+  await updateDoc(docRef, {
+    userName: "userName"
+  });
 }
