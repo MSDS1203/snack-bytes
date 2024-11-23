eb1a903db34dfe492a59feff926d4da2128d3941
Kailee Grubbs <kailee.grubbs@gmail.com>
2024-11-23 16:52:48 -0500

    Update home page to check for cookies

--- themes.js
+++ themes.js
@@ -1,3 +1,36 @@
+/***** Parse existing cookies *****/
+
+let cookie = document.cookie;
+console.log(cookie);
+
+let theme = parseTheme(cookie);
+console.log(theme);
+
+setTheme(theme); 
+
+function parseTheme(cookie) {
+    let cookieArray = cookie.split(';'); //Split into an array of cookies
+
+    for (i = 0; i < cookieArray.length; i++)
+    {
+        let keyValue = cookieArray[i].split('='); //Split each cookie into key-value pairs
+        if (keyValue[0] === "theme") {
+            return keyValue[1];
+        }
+    }
+
+    return "standard";
+    }
+
+    function setTheme(theme) {
+    console.log("Change to theme " + theme);
+    document.body.className = "";
+    document.body.classList.add(theme);
+}
+
+
+/***** Set new cookies *****/
+
 // Get each button in the dropdown menu
 let standard = document.getElementById("standard-theme");
 let dark = document.getElementById("dark-mode");
@@ -20,7 +53,9 @@ function updateTheme() {
     console.log("Change to theme " + this.theme);
     document.body.className = "";
     document.body.classList.add(this.theme);
-    document.cookie = "theme=" + this.theme + "; path=./Minesweeper/minesweeper.html";
+    document.cookie = "theme=" + this.theme;
 }
+
+
 
 
