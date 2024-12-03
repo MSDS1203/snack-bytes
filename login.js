5c8e5e1dfeed68796ed589b21e50f9cc089659b9
srazam <srazam5@gmail.com>
2024-12-03 10:35:34 -0500

    cleaning up login.js

--- login.js
+++ login.js
@@ -1,10 +1,8 @@
-//https://firebase.google.com/docs/firestore/manage-data/add-data
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { doc, setDoc, getFirestore, getDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
+// Firebase configurations
 const firebaseConfig = {
     apiKey: "AIzaSyDcTqPERyxlIKB3CCDY6BgX3tsr6S8UIzU",
     authDomain: "snack-bytes.firebaseapp.com",
@@ -14,11 +12,12 @@ const firebaseConfig = {
     appId: "1:149505884441:web:6aff0e6f5b65d453350588"
 };
 
+// Initializing Firebase application
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Getting buttons and containers
 const submitButton = document.getElementById("submit");
 const signupButton = document.getElementById("sign-up");
 const emailInput = document.getElementById("email");
@@ -26,6 +25,7 @@ const passwordInput = document.getElementById("password");
 const loggingIn = document.getElementById("logging-in");
 const createacct = document.getElementById("create-acct")
 
+// Getting user information after account is created
 const signUpUserNameIn = document.getElementById("user-name");
 const signupEmailIn = document.getElementById("email-signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
@@ -33,13 +33,16 @@ const signupPasswordIn = document.getElementById("password-signup");
 const confirmSignUpPasswordIn = document.getElementById("confirm-password-signup");
 const createacctbtn = document.getElementById("create-acct-btn");
 
+// Going back to the "logging-in" container
 const returnBtn = document.getElementById("return-btn");
 
 var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword, signUpUserName;
 
+// Create an account
 createacctbtn.addEventListener("click", async function() {
   var isVerified = true;
 
+  // Getting the email and ensuring it's correct/valid
   signupEmail = signupEmailIn.value;
   confirmSignupEmail = confirmSignupEmailIn.value;
   if(signupEmail != confirmSignupEmail) {
@@ -47,6 +50,7 @@ createacctbtn.addEventListener("click", async function() {
       isVerified = false;
   }
 
+  // Getting the password and ensuring it's correct/valid
   signupPassword = signupPasswordIn.value;
   confirmSignUpPassword = confirmSignUpPasswordIn.value;
   if(signupPassword != confirmSignUpPassword) {
@@ -54,12 +58,16 @@ createacctbtn.addEventListener("click", async function() {
       isVerified = false;
   }
   
+  // Getting the user name
   signUpUserName = signUpUserNameIn.value;
+
+  // Making sure all fields are filled out
   if(signUpUserName == null || signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
     window.alert("Please fill out all fields required.");
     isVerified = false;
   }
   
+  // If all fields are filled out the password, email, and username are valid, create the account
   if (isVerified) {
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
@@ -68,8 +76,7 @@ createacctbtn.addEventListener("click", async function() {
       localStorage.setItem("userEmail", user.email);
       localStorage.setItem("userName", signUpUserName);
 
-      console.log("Attemptying to add user data to firestore:");
-      // Add user data to Firestore with user ID
+      // Create a new document in userScores collection based off the user just created
       await setDoc(doc(db, "userScores", user.uid), {
         username: signUpUserName,
         donutEasy: 0,
@@ -79,30 +86,28 @@ createacctbtn.addEventListener("click", async function() {
         snake: 0,
         solitaire: 0
       });
-      console.log("User data added to firestore");
 
+      // Initialize new documents for each leaderboard for the user
       await setDoc(doc(db, "snakeLeaderboard", user.uid), {
         username: signUpUserName,
         snake: 0,
       });
-
       await setDoc(doc(db, "flappyBatLeaderboard", user.uid), {
         username: signUpUserName,
         flappyBat: 0,
       });
-
       await setDoc(doc(db, "donutLeaderboard", user.uid), {
         username: signUpUserName,
         donutEasy: 0,
         donutMedium: 0,
         donutHard: 0,
       });
-
       await setDoc(doc(db, "solitaireLeaderboard", user.uid), {
         username: signUpUserName,
         solitaire: 0,
       });
 
+      // Go to the home page as soon as the user creates an account
       window.location.href = "home.html";
 
     } catch (error) {
@@ -112,26 +117,22 @@ createacctbtn.addEventListener("click", async function() {
 
 });
 
+// Loggin in
 submitButton.addEventListener("click", async function() {
   email = emailInput.value;
-  console.log(email);
   password = passwordInput.value;
-  console.log(password);
 
   try{
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
-      window.alert("Successfully logged in!");     
-
+      const user = userCredential.user;   
       const docRef = doc(db, "userScores", user.uid)
       const docSnap = await getDoc(docRef);
 
       if(docSnap.exists()) {
-        console.log("Document data: ", docSnap.data());
         localStorage.setItem("userName", docSnap.data()["username"]);
       }
       else{
-        console.log("document doesn't exist ");
+        window.alert("User does not exist; please create an account");
       }
 
       window.location.href = "home.html";
